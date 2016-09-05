537b531b169ab442011be583fa1591c8194286b2
memoriesofsnows <314548713@qq.com>
2016-09-05 20:24:24 +0800

    Initial Commit

---
+++ MOFSPickerManagerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MOFSPickerManagerDemo
+//
+//  Created by luoyuan on 16/9/5.
+//  Copyright © 2016年 luoyuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MOFSPickerManagerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MOFSPickerManagerDemo
+//
+//  Created by luoyuan on 16/9/5.
+//  Copyright © 2016年 luoyuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
